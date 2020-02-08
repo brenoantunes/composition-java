40bcc4be5d5df890d19a26dd33fa913b1410f8f7
Breno Mourao <brenomourao@yahoo.com.br>
2020-02-08 19:44:06 -0300

    Entities concluída

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,5 +1,73 @@
 package entities;
 
-public class Worker {
+import java.util.ArrayList;
+import java.util.List;
 
+import entities.enums.WorkerLevel;
+
+public class Worker {
+	private String name;
+	private WorkerLevel level;
+	private Double baseSalary;
+	
+	private Department department;
+	private List<HourContract> contracts = new ArrayList<>();
+	
+	public Worker() {
+	}
+
+	public Worker (String name, WorkerLevel level, Double baseSalary, Department department) {
+		this.name = name;
+		this.level = level;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public WorkerLevel getLevel() {
+		return level;
+	}
+
+	public void setLevel(WorkerLevel level) {
+		this.level = level;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+	
+	public Department setDepartment() {
+		return department;
+	}
+	
+	public void getDepartment(Department department) {
+		this.department = department;
+	}
+	
+	public List<HourContract> getContracts(){
+		return contracts;
+	}
+	
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
+	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
+	
+	
+	
+	
 }
