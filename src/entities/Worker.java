705030dfecf81b826953ad57cf87e0f045c4837b
Breno Mourao <brenomourao@yahoo.com.br>
2020-02-14 23:24:49 -0300

    Projeto terminado

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,6 +1,7 @@
 package entities;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import entities.enums.WorkerLevel;
@@ -47,12 +48,12 @@ public class Worker {
 		this.baseSalary = baseSalary;
 	}
 	
-	public Department setDepartment() {
-		return department;
+	public void setDepartment(Department department) {
+		this.department = department;
 	}
 	
-	public void getDepartment(Department department) {
-		this.department = department;
+	public Department getDepartment() {
+		return department;
 	}
 	
 	public List<HourContract> getContracts(){
@@ -66,8 +67,18 @@ public class Worker {
 	public void removeContract(HourContract contract) {
 		contracts.remove(contract);
 	}
-	
-	
-	
-	
+		
+	public double income(int year, int month) {
+		double sum = baseSalary;
+		Calendar cal = Calendar.getInstance();
+		for (HourContract c : contracts) {
+			cal.setTime(c.getDate());
+			int c_year = cal.get(Calendar.YEAR);
+			int c_month = 1 + cal.get(Calendar.MONTH);
+			if (year == c_year && month == c_month) {
+				sum += c.totalValue();
+			}
+		}
+		return sum;
+	}
 }
